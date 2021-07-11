c58e5cb957de768690e709e0879777c45d93f489
Jonathan Cano <jcano528@gmail.com>
2021-07-10 23:53:34 -0500

    Created a working about section

--- testing-assets/js/testing.js
+++ testing-assets/js/testing.js
@@ -1,12 +1,51 @@
-/* use javascript to dynamically generate another villain to add to this list */
+let panorama;
 
+function initMap() {
+  const astorPlace = { lat: 40.729884, lng: -73.990988 };
+  // Set up the map
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: astorPlace,
+    zoom: 18,
+    streetViewControl: false,
+  });
+  document.getElementById("toggle").addEventListener("click", toggleStreetView);
+  // Set up the markers on the map
+  const cafeMarker = new google.maps.Marker({
+    position: { lat: 40.730031, lng: -73.991428 },
+    map,
+    icon: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe|FFFF00",
+    title: "Cafe",
+  });
+  const bankMarker = new google.maps.Marker({
+    position: { lat: 40.729681, lng: -73.991138 },
+    map,
+    icon: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=dollar|FFFF00",
+    title: "Bank",
+  });
+  const busMarker = new google.maps.Marker({
+    position: { lat: 40.729559, lng: -73.990741 },
+    map,
+    icon: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=bus|FFFF00",
+    title: "Bus Stop",
+  });
+  // We get the map's default panorama and set up some defaults.
+  // Note that we don't yet set it visible.
+  panorama = map.getStreetView(); // TODO fix type
+  panorama.setPosition(astorPlace);
+  panorama.setPov(
+    /** @type {google.maps.StreetViewPov} */ {
+      heading: 265,
+      pitch: 0,
+    }
+  );
+}
 
-var listEl = document.querySelector("#villains");
+function toggleStreetView() {
+  const toggle = panorama.getVisible();
 
-var villainListEl = document.querySelector(".list-group-item li");
-
-villainListEl.textContent ="The Red Queen";
-
-villainListEl.className ="list-group-item";
-
-listEl.appendChild(villainListEl);
+  if (toggle == false) {
+    panorama.setVisible(true);
+  } else {
+    panorama.setVisible(false);
+  }
+}
